b0cfc30c5514600f5765af78cfa3efcb691d117e
MickenCZ <MickenCZseznam.cz>
2022-11-26 12:22:33 +0100

    animation logic

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,16 +1,41 @@
 class Sprite {
-  constructor({position, imageSrc}) {
+  constructor({position, imageSrc, scale = 1, framesMax = 1}) {
     this.position = position
     this.width = 50
     this.height = 150
     this.image = new Image()
     this.image.src = imageSrc
+    this.scale = scale
+    this.framesMax = framesMax
+    this.framesCurrent = 0
+    this.framesElapsed = 0
+    this.framesHold = 10
   }
 
   draw() {
-    ctx.drawImage(this.image, this.position.x, this.position.y)
+    ctx.drawImage(
+      this.image,
+      this.framesCurrent * (this.image.width / this.framesMax), //animate when drawing using cropping series of images
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      (this.image.width / this.framesMax) * this.scale, //ensures scaling from class args
+      this.image.height * this.scale,
+      )
   }
   update() {
     this.draw()
+    this.framesElapsed++
+    if (this.framesElapsed % this.framesHold === 0) {
+      //for every nth frame (n = framesHold) it carries out the animation
+      if (this.framesCurrent < this.framesMax - 1) {
+        //so that it doesnt go back from lack of frames in sprite and ends at the last one
+        this.framesCurrent++
+      } else {
+        this.framesCurrent = 0
+      }
+    }
   }
 }
